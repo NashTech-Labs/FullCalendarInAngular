a019d61ad8a7b004091d77e98794c17b34366b2c
kiran-0407 <kiran.kaur@knoldus.com>
2022-07-24 22:41:52 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calendarIntegration';
+  modalRef: BsModalRef | undefined;
+  @ViewChild('viewModal')viewModal: TemplateRef<any>;
+  @ViewChild('deleteModal')deleteModal: TemplateRef<any>;
+  eventName: string = "";
+  date:any;
+  newEvents = [];
+  deleteEventTitle: string ="";
+
+  constructor(private modalService: BsModalService){
+  }
+
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    weekends: true,
+    dateClick: this.onDateClick.bind(this),
+    events: [],
+    eventClick: this.handleEventClick.bind(this),
+  };
+
+  onDateClick(date: { dateStr: string; }) {
+    this.modalRef = this.modalService.show(this.viewModal);
+    this.date = date.dateStr;
+  }
+
+  createEvent() {
+    let newEvent = {
+      title: this.eventName,
+      date: this.date
+    };
+    this.newEvents.push(newEvent);
+    this.calendarOptions.events = [...this.newEvents];
+    this.modalService.hide();
+    this.eventName = "";
+      
+  }
+
+  handleEventClick(arg){
+    this.deleteEventTitle = arg.event._def.title;
+    this.modalRef = this.modalService.show(this.deleteModal);
+  }
+
+  deleteEvent(arg){
+    for (var i = 0; i < this.newEvents.length; i++) {
+      if (this.newEvents[i].title == this.deleteEventTitle) {
+        this.newEvents.splice(i, 1);
+        this.calendarOptions.events=[];
+        break;
+      }
+    }
+    this.calendarOptions.events = [...this.newEvents];
+    this.modalService.hide();
+  }
+
+  cancelDialog(){
+    this.eventName = "";
+    this.modalService.hide();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import interactionPlugin from '@fullcalendar/interaction';
+import { BsModalService, ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule } from '@angular/forms';
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [
@@ -10,6 +20,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    FullCalendarModule,
+    ModalModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
